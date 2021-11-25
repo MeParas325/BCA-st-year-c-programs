b29086cd29820a74e5b9b025a0e7ac4682276202
MeParas325 <adamchacha398@gmail.com>
2021-11-25 06:38:21 +0530

    Added some c programs

---
+++ CBNST/Bisection.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+
+double fun(double x){
+    return x*x*x-4*x-9;
+}
+int main(){
+    int i = 0;
+    double x0, x1=0, x2;
+
+    while(fun(x1)<0)
+        x1++;
+    
+    x0 = x1-1;
+
+    do
+    {
+        x2 = (x1+x0)/2.0;
+        if(fun(x0)*fun(x2)<0){
+            x1 = x2;
+        }
+        else{
+            x0 = x2;
+        }
+    i = i+1;
+    printf("\nThe value in %d iteration is:%.4lf",i,x2);  
+    } while (fabs(x0-x1)>0.0001);
+    printf("\nThe value in first iteration is:%.4lf",x2);  
+    return 0;
+    
+}
